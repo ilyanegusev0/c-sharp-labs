e47be09bca09592b2755293c37a5cffcafa54886
ilyanegusev0 <ilyanegusev0912@gmail.com>
2025-10-04 13:31:35 +0300

    add 'HospitalManagementSystem/'

---
+++ README.md
@@ -0,0 +1,8 @@
+# Лабораторна робота #2
+
+**Виконав:** Негусєв І. В.
+
+**Група:** ПД-25
+
+# Screenshots:
+![Unit Tests 2](screenshots/unit-tests.png)
---
+++ source/HospitalManagementSystem/HospitalManagementSystem/MedicalRecord.cs
@@ -0,0 +1,18 @@
+﻿namespace HospitalManagementSystem
+{
+    public class MedicalRecord
+    {
+        public Patient Patient;
+        public Doctor Doctor;
+        public DateTime Date;
+        public string Description;
+
+        public MedicalRecord(Patient patient, Doctor doctor, DateTime date, string description)
+        {
+            Patient = patient;
+            Doctor = doctor;
+            Date = date;
+            Description = description;
+        }
+    }
+}
