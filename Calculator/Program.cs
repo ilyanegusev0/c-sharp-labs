ca1c4442b8fb0deaa7e8aa8b65fd0475bdf98db4
ilyanegusev0 <ilyanegusev0912@gmail.com>
2026-03-01 14:39:35 +0200

    Import project Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Calculator
+{
+    delegate double MathOperation(double a, double b);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mo;
+
+            double a = 5;
+            double b = 10;
+
+            mo = Add;
+            Console.WriteLine($"Add({a}, {b}): {mo(a, b)}");
+
+            mo = Subtract;
+            Console.WriteLine($"Subtract({a}, {b}): {mo(a, b)}");
+
+            mo = Multiply;
+            Console.WriteLine($"Multiply({a}, {b}): {mo(a, b)}");
+
+            mo = Divide;
+            Console.WriteLine($"Divide({a}, {b}): {mo(a, b)}");
+        }
+
+        static public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        static public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        static public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        static public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
