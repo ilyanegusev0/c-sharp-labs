4f0750d301565f337be0836d63dd55e62f5ea408
ilyanegusev0 <ilyanegusev0912@gmail.com>
2026-03-01 15:43:43 +0200

    Import project Logging

---
+++ Logging/Logger.cs
@@ -0,0 +1,12 @@
+﻿namespace Logging
+{
+    internal class Logger
+    {
+        public Action<string> LogHandler;
+
+        public void Log(string message)
+        {
+            LogHandler?.Invoke(message);
+        }
+    }
+}
---
+++ Logging/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Logging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            logger.LogHandler = msg => Console.WriteLine(msg);
+            logger.Log("It's a normal message.");
+
+            logger.LogHandler = msg => Console.WriteLine(msg.ToUpper());
+            logger.Log("It's a message in upper case.");
+        }
+    }
+}
