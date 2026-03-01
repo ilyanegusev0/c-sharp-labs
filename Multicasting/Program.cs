b1f42a8e3fa91c75694afbc5fc9b772e3744c019
ilyanegusev0 <ilyanegusev0912@gmail.com>
2026-03-01 14:53:21 +0200

    Import project Multicasting

---
+++ Multicasting/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Multicasting
+{
+    delegate void NotificationHandler(string message);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotificationHandler nh;
+
+            nh = SendEmail;
+            nh += SendSMS;
+
+            nh("Hello!");
+        }
+
+        static public void SendEmail(string message)
+        {
+            Console.WriteLine($"Email sent: {message}");
+        }
+        static public void SendSMS(string message)
+        {
+            Console.WriteLine($"SMS sent: {message}");
+        }
+    }
+}
