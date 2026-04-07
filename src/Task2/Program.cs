6057e79c243ee282d8fc77eff76731ded7621774
ilyanegusev <ilyanegusev0912@gmail.com>
2026-04-07 19:12:59 +0300

    Upload Task2 project

---
+++ src/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string OUTPUT_FILE = "students.json";
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            List<Student> serializedStudents = new List<Student>();
+
+            serializedStudents.Add(new Student("Alex", 17));
+            serializedStudents.Add(new Student("Jhon", 15));
+            serializedStudents.Add(new Student("Alice", 16));
+            serializedStudents.Add(new Student("Harry", 13));
+            serializedStudents.Add(new Student("Mary", 14));
+
+            string json = JsonSerializer.Serialize(serializedStudents, options);
+            File.WriteAllText(OUTPUT_FILE, json);
+
+            List<Student>? deserializedStudent = new List<Student>();
+
+            json = File.ReadAllText(OUTPUT_FILE);
+            deserializedStudent = JsonSerializer.Deserialize<List<Student>>(json, options) ?? new List<Student>();
+
+            Console.WriteLine($"STUDENTS ({deserializedStudent.Count}):");
+            foreach (var student in deserializedStudent)
+                Console.WriteLine($" - {student.Name} | AGE: {student.Age} | AVG: {student.AverageScore}");
+        }
+    }
+}
---
+++ src/Task2/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace Task2
+{
+    internal class Student
+    {
+        private static Random _random = new Random();
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double AverageScore { get; set; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            AverageScore = _random.Next(500, 1000) / 10.0;
+        }
+    }
+}
